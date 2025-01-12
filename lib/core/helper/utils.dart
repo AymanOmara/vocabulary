8e18198c5ee465e599c2a35742fe8a499e73f8ee
“aomara” <aymanomara55@gmail.com>
2025-01-12 08:50:38 +0200

    add visuals to home screen

--- lib/core/helper/utils.dart
+++ lib/core/helper/utils.dart
@@ -1,13 +1,5 @@
 import 'package:vocabulary/core/display/i_selectable.dart';
 
-extension ArrayUtils<T> on List<T> {
-  void updateWhere(bool Function(T) condition, T newItem) {
-    final index = indexWhere(condition);
-    if (index != -1) {
-      this[index] = newItem;
-    }
-  }
-}
 extension MarkUnselected on List<ISelectable> {
   void unSelectAllExpect(String key) {
     for (var i in this) {
--- lib/features/home/presentation/page/home_screen.dart
+++ lib/features/home/presentation/page/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vocabulary/core/routing/app_routes.dart';
+import 'package:vocabulary/core/ui/theme/colors.dart';
 import 'package:vocabulary/features/home/business_logic/home_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,6 +18,7 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           body: SafeArea(
             child: Column(
+              mainAxisSize: MainAxisSize.max,
               children: [
                 SizedBox(
                   height: 10,
@@ -34,52 +37,77 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                CarouselSlider.builder(
-                  itemCount: cubit.vocabularies.length,
-                  itemBuilder: (context, index, realIndex) {
-                    final word = cubit.vocabularies[index];
-                    return InkWell(
-                      onTap: () {
-                        Navigator.of(context).pushNamed(
-                          AppRoutes.vocabularyDetails,
-                          arguments: word.word,
-                        );
-                      },
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              word.word,
-                              style: const TextStyle(
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            Text(
-                              word.definition,
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                            const SizedBox(height: 16),
-                            Text(
-                              word.exampleSentence,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                          ],
+                Padding(
+                  padding: EdgeInsets.all(
+                    20,
+                  ),
+                  child: CarouselSlider.builder(
+                    itemCount: cubit.vocabularies.length,
+                    itemBuilder: (context, index, realIndex) {
+                      final word = cubit.vocabularies[index];
+                      return Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 10,
                         ),
-                      ),
-                    );
-                  },
-                  options: CarouselOptions(
-                    enableInfiniteScroll: true,
-                    autoPlay: false,
-                    viewportFraction: 1.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: linkColor,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              20,
+                            ),
+                          ),
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pushNamed(
+                              AppRoutes.vocabularyDetails,
+                              arguments: word.word,
+                            );
+                          },
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  word.word,
+                                  style: const TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 16),
+                                Text(
+                                  word.definition,
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                                const SizedBox(height: 16),
+                                Text(
+                                  word.exampleSentence,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    options: CarouselOptions(
+                      enableInfiniteScroll: true,
+                      autoPlay: false,
+                      viewportFraction: 1.0,
+                    ),
                   ),
                 ),
+                Spacer(),
+                SvgPicture.asset(
+                  "assets/wave.svg",
+                  fit: BoxFit.fill,
+                )
               ],
             ),
           ),
